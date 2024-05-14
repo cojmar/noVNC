9d5ee08b9a79e9388b4396f361980c1e0b95126d
coj <cojmar@gmail.com>
2024-05-14 11:15:02 +0000

    .

--- index.js
+++ index.js
@@ -49,7 +49,12 @@ app.use((req, res, next) => {
 })
 
 app.use('/', express.static(path.join(__dirname, 'noVNC')))
-app.get('/', (req, res) => res.redirect(301, '/vnc.html'))
+app.get('/', (req, res) => {
+    // return res.redirect(301, 'vnc.html')
+    res.set('Content-Type', 'text/html')
+    res.write(fs.readFileSync('noVNC/vnc.html'))
+    res.end()
+})
 server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head))
 
 websockify({
